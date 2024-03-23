525e07e33eb6d8f16e775c90088877771e21d6d6
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-03-23 03:55:47 +0100

    1-install_a_package.pp

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,16 @@
+# Using Puppet to install flask from pip3
+
+exec { 'apt-get update':
+    command => '/usr/bin/apt-get update'
+}
+
+package { 'python3-pip':
+  ensure => 'installed',
+  require => Exec['apt-get update']
+}
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip']
+}
