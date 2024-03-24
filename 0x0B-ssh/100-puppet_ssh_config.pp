2ffdfbc3365284a32b2487834644d9b7de19d6c3
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-03-24 19:18:54 +0100

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # Puppet script to create ssh config file
 
-file_line { 'Turn off passwd auth':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
-}
-
-file_line { 'Declare identity file':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '    IdentityFile ~/.ssh/school',
+file { '/home/ermac/.ssh/config':
+  ensure  => present,
+  content => "
+    Host *
+        IdentityFile ~/.ssh/school
+        PasswordAuthentication no
+  ",
+  mode    => '0600',
+  owner   => 'ermac',
+  group   => 'ermac',
 }
