fcedef0374417b88ed489c6e0d7c1d330283e324
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-03-24 18:54:26 +0100

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,14 @@
-# Sharing my SSH client configuration for working without typing a password.
+# Sharing my SSH client configuration for working without typing a password
+# using Puppet.
 
-file { '/home/ermac/.ssh/config':
-  ensure  => present,
-  content => "
-    Host *
-        IdentityFile ~/.ssh/school
-        PasswordAuthentication no
-  "
+file_line { 'Turn off passwd auth':
+  path    => '/home/ermac/.ssh/config',
+  line    => 'PasswordAuthentication no',
+  match   => '^#*PasswordAuthentication',
+}
+
+file_line { 'Declare identity file':
+  path    => '/home/ermac/.ssh/config',
+  line    => 'IdentityFile ~/.ssh/school',
+  match   => '^#*IdentityFile',
 }
