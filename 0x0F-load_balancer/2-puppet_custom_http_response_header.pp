baae7cbd4205ddd2d4cb19bd0015220f895c66ff
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-04-02 03:50:45 +0100

    2-puppet_custom_http_response_header.pp

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,23 @@
+exec { 'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
+}
+
+exec { 'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
+}
+
+exec { 'add_header':
+  provider    => shell,
+  environment => ["HOST=${hostname}"],
+  command     => "sudo sed -i '/http {/a \\ \\ \\ \\ \\ \\ \\ \\ add_header X-Served-By $HOSTNAME;' /etc/nginx/nginx.conf",
+  before      => Exec['restart Nginx'],
+}
+
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
