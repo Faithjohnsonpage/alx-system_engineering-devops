07009959092e93499e55f59913efb470ec742f9b
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-04-22 20:06:44 +0100

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,29 +5,30 @@ import requests
 import sys
 
 
-response1 = requests.get('https://jsonplaceholder.typicode.com/users/')
-response2 = requests.get('https://jsonplaceholder.typicode.com/todos/')
-response1 = response1.json()
-response2 = response2.json()
+if __name__ == '__main__':
+    response1 = requests.get('https://jsonplaceholder.typicode.com/users/')
+    response2 = requests.get('https://jsonplaceholder.typicode.com/todos/')
+    response1 = response1.json()
+    response2 = response2.json()
 
-employee_id = int(sys.argv[1])
+    employee_id = int(sys.argv[1])
 
-total_tasks = 0
-task_completed = 0
+    total_tasks = 0
+    task_completed = 0
 
-for task in response2:
-    if task.get('userId') == employee_id:
-        total_tasks += 1
-        if task.get('completed') is True:
-            task_completed += 1
+    for task in response2:
+        if task.get('userId') == employee_id:
+            total_tasks += 1
+            if task.get('completed') is True:
+                task_completed += 1
 
-for user in response1:
-    if user.get('id') == employee_id:
-        user_info = "Employee {} is done with tasks({}/{}):".\
-            format(user.get('name'), task_completed, total_tasks)
+    for user in response1:
+        if user.get('id') == employee_id:
+            user_info = "Employee {} is done with tasks({}/{}):".\
+                format(user.get('name'), task_completed, total_tasks)
 
-print(user_info)
-for task in response2:
-    if task.get('userId') == employee_id:
-        if task.get('completed') is True:
-            print("\t {}".format(task.get('title')))
+    print(user_info)
+    for task in response2:
+        if task.get('userId') == employee_id:
+            if task.get('completed') is True:
+                print("\t {}".format(task.get('title')))
