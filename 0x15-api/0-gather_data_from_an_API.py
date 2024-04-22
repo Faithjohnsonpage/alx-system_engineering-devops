483e8369f58ec6300c8027000be4c0977c50d351
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-04-22 19:56:33 +0100

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This script uses a REST API  for a given employee ID,
+returns information about his/her TODO list progress."""
+import requests
+import sys
+
+
+response1 = requests.get('https://jsonplaceholder.typicode.com/users/')
+response2 = requests.get('https://jsonplaceholder.typicode.com/todos/')
+response1 = response1.json()
+response2 = response2.json()
+
+employee_id = int(sys.argv[1])
+
+total_tasks = 0
+task_completed = 0
+
+for task in response2:
+    total_tasks += 1
+    if task.get('userId') == employee_id:
+        if task.get('completed') == True:
+            task_completed += 1
+
+for user in response1:
+    if user.get('id') == employee_id:
+        user_info = "Employee {} is done with tasks({}/{}):".\
+            format(user.get('name'), task_completed, total_tasks)
+
+print(user_info)
+for task in response2:
+    if task.get('userId') == employee_id:
+        if task.get('completed') == True:
+            print("\t {}".format(task.get('title')))
