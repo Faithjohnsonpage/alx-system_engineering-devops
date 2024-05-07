88eeb06a21cc7b673b9317d834085b4b18f8fb58
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-05-07 18:49:13 +0100

    First commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Custom User Agent'}
+    headers = {'User-Agent': 'Google Chrome Version 81.0.4044.129'}
     r = requests.get(url, headers=headers, allow_redirects=False)
     if r.status_code == 200:
         data = r.json()
