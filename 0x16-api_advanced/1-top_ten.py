14bc60fbf1e784c0b07d7adc221eafa7786e1da2
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-05-09 16:08:00 +0100

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,10 +13,11 @@ def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-agent': 'My API'}
     params = {'limit': 10}
-    r = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    r = requests.get(url, headers=headers, params=params,
+                     allow_redirects=False)
     if r.status_code == 200:
         posts = r.json()['data']['children']
-        for post in posts:
-                print(post['data']['title'])
+    for post in posts:
+        print(post['data']['title'])
     else:
         print('None')
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,28 +1,16 @@
 #!/usr/bin/python3
-"""
-Using reddit's API
-"""
+"""This module queries the Reddit API"""
 import requests
-after = None
 
 
 def recurse(subreddit, hot_list=[]):
-    """returning top ten post titles recursively"""
-    global after
-    user_agent = {'User-Agent': 'api_advanced-project'}
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    parameters = {'after': after}
-    results = requests.get(url, params=parameters, headers=user_agent,
-                           allow_redirects=False)
+    """A recursive function that queries the Reddit API and returns a list
+    containing the titles of all hot articles for a given subreddit."""
+    if subreddit is None or not isinstance(subreddit, str):
+        return None
 
-    if results.status_code == 200:
-        after_data = results.json().get("data").get("after")
-        if after_data is not None:
-            after = after_data
-            recurse(subreddit, hot_list)
-        all_titles = results.json().get("data").get("children")
-        for title_ in all_titles:
-            hot_list.append(title_.get("data").get("title"))
-        return hot_list
-    else:
-        return (None)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-agent': 'My API'}
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    if r.status_code == 200:
+        
