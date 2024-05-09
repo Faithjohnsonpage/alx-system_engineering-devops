b3c5a0aed8f6d83c9006b5450229ba7bc080b762
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-05-09 15:28:50 +0100

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,33 +1,22 @@
 #!/usr/bin/python3
-
-"""
-prints the titles of the first 10 hot posts listed for a given subreddit
-"""
-
-from requests import get
+"""This module queries the Reddit API"""
+import requests
+import sys
 
 
 def top_ten(subreddit):
-    """
-    function that queries the Reddit API and prints the titles of the first
-    10 hot posts listed for a given subreddit
-    """
+    """This function that queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit."""
 
     if subreddit is None or not isinstance(subreddit, str):
-        print("None")
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+        print('None')
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-agent': 'My API'}
     params = {'limit': 10}
-    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-
-    response = get(url, headers=user_agent, params=params)
-    results = response.json()
-
-    try:
-        my_data = results.get('data').get('children')
-
-        for i in my_data:
-            print(i.get('data').get('title'))
-
-    except Exception:
-        print("None")
+    r = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    if r.status_code == 200:
+        posts = r.json()['data']['children']
+        for post in posts:
+                print(post['data']['title'])
+    else:
+        print('None')
